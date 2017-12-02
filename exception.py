db1e9887bfadff99cb451ea60c4db95fc6404c96
pabfor <pablo.torreblanca-alarie.1@ulaval.ca>
2017-12-01 20:58:12 -0500

    ajout d'un fichier .py contenant les classes d'exceptions.

---
+++ exception.py
@@ -0,0 +1,30 @@
+"""
+Fichier python contenant les classes d'exeption. J'ai pensé à les mettres dans un fichier à part.
+
+"""
+
+
+class PositionInvalideException(Exception):
+    pass
+    # TODO
+
+
+class MotNonPermisException(Exception):
+    pass
+    # TODO
+
+
+class CaseOccupeeException(Exception):
+    pass
+    # TODO
+
+
+class CaseVideException(Exception):
+    pass
+    # TODO
+
+
+class AutreExeption(Exception):
+    pass
+    # TODO
+
